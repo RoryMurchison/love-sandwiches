45b887859a2f98fdf7dd10d1d0eac13da56fbf5c
RoryMurchison <roryjmurchison@gmail.com>
2023-01-13 19:59:24 +0000

    calculate surplus data

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
@@ -13,6 +12,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 def get_sales_data():
     """
@@ -36,6 +36,7 @@ def get_sales_data():
 
     return sales_data
 
+
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
@@ -53,6 +54,7 @@ def validate_data(values):
         return False
 
     return True
+
 
 def update_sales_worksheet(data):
     """
@@ -74,7 +76,13 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
 
 
 def main():
@@ -84,6 +92,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches Data Automation")
